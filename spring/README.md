ca76ab5846d1e3ec6ae1e542b7e719e64b220b55
cucker <hanxiao2100@qq.com>
2020-06-17 18:46:17 +0800

    add spring proj

---
+++ spring/README.md
@@ -0,0 +1,25 @@
+# Spring
+
+
+## 什么是Spring
+```text
+Spring是一个开源的java框架。
+Spring为简化企业级应用开发而生。
+Spring是一个IOC(DI)和AOP容器框架。
+
+特点：
+* 轻量级
+    Spring是非侵入性的，基于spring开发的应用，其对象可以不依赖spring的API
+* 依赖注入
+    DI：dependency injection，依赖注入、
+    IOC：Inversion of Control，控制反转
+* 面向切面编程
+    AOP：aspect oriented programming
+* 容器
+    Spring 是一个容器, 因为它包含并且管理应用对象的生命周期
+* 框架
+    Spring 实现了使用简单的组件配置组合成一个复杂的应用. 在 Spring 中可以使用 XML 和 Java 注解组合这些对象
+* 一站式
+    在 IOC 和 AOP 的基础上可以整合各种企业应用的开源框架和优秀的第三方类库 
+    （实际上 Spring 自身也提供了展现层的 SpringMVC 和 持久层的 Spring JDBC）
+```
---
+++ spring/src/com/java/bean/User.java
@@ -0,0 +1,62 @@
+package com.java.bean;
+
+public class User {
+    private String name;
+    private int age;
+    // gende, 0: female, 1: male
+    private int sex;
+
+    // 构造器
+    public User() {}
+
+    public User(String name, int age, int sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    // 方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String info() {
+        String s = null;
+        if (sex == 0) {
+            s = "这是位美如天仙的女士";
+        } else {
+            s = "这是位气度不凡的绅士";
+        }
+        System.out.println(s);
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+}
