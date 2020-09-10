654c4544edc69a17db590eda0ab924061ec0d45c
cucker <hanxiao2100@qq.com>
2020-09-10 18:04:55 +0800

    add mybatis5 dynamic sql

---
+++ MyBatis/mybatis5/src/com/java/dao/EmployeeDynamicSqlMapper.java
@@ -0,0 +1,13 @@
+package com.java.dao;
+
+import com.java.bean.Employee;
+
+import java.util.List;
+
+public interface EmployeeDynamicSqlMapper {
+
+    // 根据传入的employee对象，查与此对象有值的属性都符合的员工信息。多个属性之间为and关系
+    List<Employee> getEmployeesByConditionIf(Employee employee);
+
+
+}
