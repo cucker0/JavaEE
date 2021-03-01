df2ae77831212678f59ec3f8c076e3db58eb2092
cucker <hanxiao2100@qq.com>
2021-03-01 16:03:13 +0800

    init project

---
+++ SpringBoot/springboot-quick/src/main/java/com/java/springbootquick/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.java.springbootquick.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// @ResponseBody
+// @Controller
+
+/**
+ * @RestController 可以代替 包含了 @ResponseBody、@Controller
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World, quick";
+    }
+}
