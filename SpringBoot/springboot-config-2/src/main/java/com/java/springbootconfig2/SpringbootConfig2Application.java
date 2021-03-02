753f1d3b34a9bf8d450e7b94757650682da02b5f
cucker <hanxiao2100@qq.com>
2021-03-02 11:16:28 +0800

    add springboot-config-2

---
+++ SpringBoot/springboot-config-2/src/main/java/com/java/springbootconfig2/SpringbootConfig2Application.java
@@ -0,0 +1,13 @@
+package com.java.springbootconfig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConfig2Application.class, args);
+    }
+
+}
---
+++ SpringBoot/springboot-config-2/src/test/java/com/java/springbootconfig2/SpringbootConfig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springbootconfig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
