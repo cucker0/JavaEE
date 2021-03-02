6d9f742c8a5e60a666c1910f491c542d1471821d
cucker <hanxiao2100@qq.com>
2021-03-02 10:42:36 +0800

    spring boot配置文件

--- SpringBoot/springboot-config/src/test/java/com/java/springbootconfig/SpringbootConfigApplicationTests.java
+++ SpringBoot/springboot-config/src/test/java/com/java/springbootconfig/SpringbootConfigApplicationTests.java
@@ -1,13 +1,25 @@
 package com.java.springbootconfig;
 
+import com.java.springbootconfig.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootConfigApplicationTests {
+    @Autowired
+    Person person;
+
+    @Autowired
+    ApplicationContext ioc;
 
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
