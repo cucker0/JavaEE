b1f5f8a8182c37d5f19603f960f4ff21934db886
cucker <hanxiao2100@qq.com>
2021-05-18 17:40:20 +0800

    add project: springboot-listener, springboot-starter

---
+++ SpringBoot/springboot-starter/springboot-starter-autoconfigurer/src/main/java/com/java/starter/springbootstarter/SpringbootStarterAutoconfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.java.starter.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterAutoconfigurerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStarterAutoconfigurerApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/springboot-starter/springboot-starter-autoconfigurer/src/test/java/com/java/starter/springbootstarter/SpringbootStarterAutoconfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.starter.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStarterAutoconfigurerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
