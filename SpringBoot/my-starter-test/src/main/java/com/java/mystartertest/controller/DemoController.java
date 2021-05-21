d9389b7bf85f9bfcd6400516265a8347506b54e8
cucker <hanxiao2100@qq.com>
2021-05-21 17:09:48 +0800

    update mystartertest, 测试自定义的starter

--- SpringBoot/my-starter-test/src/main/java/com/java/mystartertest/controller/DemoController.java
+++ SpringBoot/my-starter-test/src/main/java/com/java/mystartertest/controller/DemoController.java
@@ -1,6 +1,6 @@
 package com.java.mystartertest.controller;
 
-import com.java.starter.springbootstarter.service.TalkService;
+import com.java.starter.service.TalkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
